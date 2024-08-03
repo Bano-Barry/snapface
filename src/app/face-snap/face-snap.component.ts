b3168e69541e875471a83ebeec58535f8afe4e14
Bano-Barry <barrymamadoubano14@gmail.com>
2024-08-03 18:13:26 +0200

    reagir aux evenements depuis le template principal

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,13 +12,34 @@ export class FaceSnapComponent {
   description!: string;
   createdAt!: Date;
   snaps!: number;
-  imageUrl! : string;
+  imageUrl!: string;
+  snapButtonText: string = 'Oh Snap!';
+  userHasSnapped!: boolean;
   ngOnInit() {
     this.title = 'Archibald';
     this.description = 'Mon meilleur ami depuis toujours !';
     this.createdAt = new Date();
     this.snaps = 5;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/16/03/teddy-bear-792273_1280.jpg';
-    
+    this.imageUrl =
+      'https://cdn.pixabay.com/photo/2015/05/16/03/teddy-bear-792273_1280.jpg';
+  }
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+    this.snaps--;
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap() {
+    this.snaps++;
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnapped = true;
   }
 }
